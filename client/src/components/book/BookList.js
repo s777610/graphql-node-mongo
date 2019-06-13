215b99af851a632b5f18bc19596a3fe7eb877963
Wei Cheng Hung <s777610@gmail.com>
2019-06-12 21:32:34 -0700

    call graphql api from client and fetch data back to client

--- client/src/components/book/BookList.js
+++ client/src/components/book/BookList.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
-const BookList = () => {
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+const BookList = ({ data }) => {
+  const displayBooks = () => {
+    if (data.loading) {
+      return <div>Loading books ...</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  };
+
   return (
     <div>
-      <ul id="book-list">
-        <li>Book List</li>
-      </ul>
+      <ul id="book-list">{displayBooks()}</ul>
     </div>
   );
 };
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -3,8 +3,12 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const cors = require("cors");
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 //////////////////////
 // Connect to Mongo //
